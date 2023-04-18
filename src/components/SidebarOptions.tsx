7d283b055eee2d66e43f2829aedd26618ea60ccd
Alim <52186295+alimalim77@users.noreply.github.com>
2023-04-18 19:01:41 +0530

    Commit

---
+++ src/components/SidebarOptions.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./SidebarOptions.css";
+
+type SidebarOptionsProps = {
+  Icon: React.ElementType;
+  text: string;
+};
+
+const SidebarOptions = ({ Icon, text }: SidebarOptionsProps) => {
+  return (
+    <div className="sidebar-options">
+      <Icon />
+      <h2>{text}</h2>
+    </div>
+  );
+};
+
+export default SidebarOptions;
